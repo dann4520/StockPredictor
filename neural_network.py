bc6b661570ae2d87bcd752f6337bf2c77e048168
dann4520 <stabdan@gmail.com>
2017-12-03 18:30:51 -0600

    Added and Ran AMZN data

--- neural_network.py
+++ neural_network.py
@@ -19,10 +19,10 @@ class NeuralNetwork:
 
     # modifier to account for input and output layer. Added to hidden_layers to give total number of layers.
     OUTSIDE_LAYERS = 2
-    WEIGHTS_FILE = "CVXWeights.csv"
-    TRAIN_FILE = "Training Data/CVX.csv"
-    TEST_FILE = "Test Data/CVX.csv"
-    OUTPUT_FILE = "Test Data/CVXResults.csv"
+    WEIGHTS_FILE = "AMZNWeights.csv"
+    TRAIN_FILE = "Training Data/AMZN.csv"
+    TEST_FILE = "Test Data/AMZN.csv"
+    OUTPUT_FILE = "Test Data/AMZNResults.csv"
     LEARNING_RATE = .005
 
     # initialize neuron with random weights
